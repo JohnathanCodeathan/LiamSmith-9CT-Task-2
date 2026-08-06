3cd1559a6fc488eaf1e185948cec9d4fffef499a
Liam.S <liam.smith121@education.nsw.gov.au>
2026-08-06 11:10:07 +1000

    Begun the code
    I do indubitably and outdubitably believe that I have begun the harrowing task of this programabular code through the menu.

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
-print("Hello World")
+from machine import Pin, ADC
+JohnButton = Pin(16, Pin.IN, Pin.PULL_DOWN)
+JaneButton = Pin(28, Pin.IN, Pin.PULL_DOWN)
+PotentialMan = ADC(Pin(22))
+def Menu():
+    if JohnButton
+def Song():
+    pass
+while True:
+    Menu()
+    Song()
