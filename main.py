dbddc6cd12f07875921138bc0b1f1e69a83c976b
K-9Krazy <liam.smith121@education.nsw.gov.au>
2026-08-11 21:22:47 +1000

    Finished Lights Function
    I have finished the lights function, which is the function responsible for controlling the lights during the level phase.  Furthermore, I've also done a bit of deviation from the plan to prevent any potential bugs that may come with the flawed plan. In the future, I will need to do the buzzer and sensor functions and actually test the code. After that, I'll need to write my evaluation and edit some variable names and github logs for easier readability, but the circuit comes first.

--- main.py
+++ main.py
@@ -17,29 +17,31 @@ JohnButton = Pin(16, Pin.IN, Pin.PULL_DOWN)# Left Button
 JaneButton = Pin(17, Pin.IN, Pin.PULL_DOWN) # Right Button
 PotentialMan = ADC(Pin(22)) #Potentiometer
     #Lights
-JohnMellow = Pin(13, Pin.OUT)
-JohnRebt = Pin(12, Pin.OUT)
-JohnGeen = Pin(14, Pin.OUT)
-JaneMellow = Pin(10, Pin.OUT)
-JaneRebt = Pin(9, Pin.OUT)
-JaneGeen = Pin(11, Pin.OUT)
+JohnMellow = Pin(10, Pin.OUT)
+JohnRebt = Pin(11, Pin.OUT)
+JohnGeen = Pin(9, Pin.OUT)
+JaneMellow = Pin(13, Pin.OUT)
+JaneRebt = Pin(14, Pin.OUT)
+JaneGeen = Pin(12, Pin.OUT)
+mod = 0.0
 manypickles = 5#Also setup for lorikeet
 pickles = Neopixel(manypickles, 0, 28, "GRB") #Set up for lorikeet
 MAHORAGA = 1.0 #Analog value of potentiometer
 Fin = False #If Level is finished
-Johntiming = 0 #Refers to the eligibility in scoring for the lights on John Buttons side
-Janetiming = 0 #Refers to the eligibility in scoring for the lights on Jane Buttons side
+Johntiming = [] #Refers to the eligibility in scoring for the lights on John Buttons side
+Janetiming = [] #Refers to the eligibility in scoring for the lights on Jane Buttons side
+#John and Jane timing are now lists because in the current order of the Lights function, it would set the timing variables to that of a red light. But if we append it all to a list, and select the biggest one. (Order is non-negotiable, refer to comment in lights function)
 corner = 0 #Tells corner lights what value to be 
 Johnreleased = False #Checks if the left button is released
 Janereleased = False#Checks if right button is released
 JohnKneaded = False #Is a note in the left side needed
 JaneKneaded = False #Is a note in the right side needed
-    #Colours
-rebt = (254, 0, 2)
-mellow = (255, 239, 1)
-suspicoussalmon = (252, 137, 119)
-geen = (155, 253, 113)
-ourple = (139, 18, 105)
+    #RGB codes for colours
+rebt = (254, 0, 2) #Red
+mellow = (255, 239, 1) #Yellow
+suspicoussalmon = (252, 137, 119) #Salmon
+geen = (155, 253, 113) #Green
+ourple = (139, 18, 105) #Purple
 score = 0
 MaxScore = 100
 Songid = 5 
@@ -117,19 +119,47 @@ def LIGHTS():
     global Johntiming
     global Janetiming
     while Fin == False:
-        if JohnKneaded == True:
-            JohnRebt.value() = 1
-            JohnKneaded = True
-        if JaneKneaded == True:
-            JaneRebt.value() = 1
-            JaneKneaded = True
-        if JohnGeen.value() ==1:
-            Johntiming = 2
+        Johntiming = []
+        Janetiming = []
+        if JohnGeen.value() ==1: #Order of the if statements in this line and below is non-negotiable because if we arrange it any other way, when say a yellow light is checked and the note goes to green, if we then check green, the previous note from the yellow will interfere with the check.
+            Johntiming.append(0)
             JohnGeen.value() = 0
         if JaneGeen.value() ==1:
-            Janetiming = 2
+            Janetiming.append(0)
             JaneGeen.value() = 0
-
+        if JohnMellow.value() ==1:
+            Johntiming.append(2)
+            JohnMellow.value() = 0
+            JohnGeen.value() = 1
+        if JaneMellow.value() ==1:
+            Janetiming.append(2)
+            JaneGeen.value() = 1
+            JaneMellow.value() = 0
+        if JohnRebt.value() ==1:
+            Johntiming.append(1)
+            JohnMellow.value() = 1
+            JohnRebt.value() = 0
+        if JaneRebt.value() ==1:
+            Janetiming.append(1)
+            JaneRebt.value() = 0
+            JaneMellow.value() = 1
+        if JohnKneaded == True:
+            JohnRebt.value() = 1
+            JohnKneaded = False
+        if JaneKneaded == True:
+            JaneRebt.value() = 1
+            JaneKneaded = False            
+        time.sleep(0.5+mod)           
+'''How LIGHTS works: 
+- Declares JohnKneaded, JaneKneaded (The variables that track whether a note is needed for the red lights in the two columns), Johntiming and Janetiming (The lists that track the scoring eligibility) to global
+Repeated until the level is finished:
+- Clear the John and Jane Timing lists
+- If the left green light is on, add 0 to the list of timing values of the left side and turn off the left green light
+- Repeat for the right green light.
+- If the left yellow light is on, add 2 to the list of timing values of the left side, turn off the left yellow light and turn on the right green light
+- Repeat for the right yellow light
+- If the left red light is on, add 1 to the list of timing values of the left side, turn off the left red light and turn on the right yellow light
+- Repeat for the right red light'''
 def CAMERA(): #Sensor
     pass
 
@@ -137,13 +167,13 @@ def ACTION(): #Button
     global corner
     while Fin == False:
         if Johnreleased == True:
-            if Johntiming == 0:
+            if max(Johntiming) == 0: #max returns the highest value from a list
                 corner = 0
-            elif Johntiming == 1:
+            elif max(Johntiming) == 1:
                 corner = 1
                 score += 1
                 JohnMellow.value() = 0
-            elif Johntiming == 2:
+            elif max(Johntiming) == 2:
                 corner = 2
                 score += 2
                 JohnGeen.value() = 0
@@ -151,13 +181,13 @@ def ACTION(): #Button
                 print("The developer is a numpty this isnt supposed to happen")
                 sys.exit()
         if Janereleased == True:
-            if Janetiming == 0:
+            if max(Janetiming) == 0:
                 corner = 0
-            elif Janetiming == 1:
+            elif max(Janetiming) == 1:
                 corner = 1
                 score += 1
                 JaneMellow.value() = 0
-            elif Janetiming == 2:
+            elif max(Janetiming) == 2:
                 corner = 2
                 score += 2
                 JaneGeen.value() = 0
